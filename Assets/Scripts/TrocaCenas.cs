f7619e7fffa0a4e822e39a2eb70b11177c3e9aca
Heribas <20233005813@estudantes.ifpr.edu.br>
2026-03-31 11:32:58 -0300

    Portas funcionam

--- Assets/Scripts/TrocaCenas.cs
+++ Assets/Scripts/TrocaCenas.cs
@@ -2,28 +2,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class TrocaCenas : MonoBehaviour
 {
+    public string JOGO1 = "SampleScene";
+    public string jogo2 = "TESTE";
+    public string jogo3 = "TESTE2";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter2D(Collider2D other) 
     {
-        if (collision.gameObject.CompareTag("Porta1"))
+        Debug.Log("aipapai");
+       
+        if (other.CompareTag("Porta1"))
         {
-            // Se for um alvo, destruímos o alvo.
-            SceneManager.LoadScene("SampleScene");
+
+            SceneManager.LoadScene(JOGO1);
         }
-        if (collision.gameObject.CompareTag("Porta2"))
+        if(other.CompareTag("Porta2"))
         {
-            // Se for um alvo, destruímos o alvo.
-            Debug.Log("Está trancada");
+            SceneManager.LoadScene(jogo2);
+        }
+        if (other.CompareTag("Porta3"))
+        {
+            SceneManager.LoadScene(jogo3);
         }
     }
 
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
         
 
